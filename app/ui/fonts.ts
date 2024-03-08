0fd09739f426e5e73cd229b46467a0af0564ae28
john ore <jore@g2o.com>
2024-03-08 16:49:44 -0500

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Work_Sans } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const work_sans = Work_Sans({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
